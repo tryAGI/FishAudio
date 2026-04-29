cc2c65521e088668cd879f0930df9946b9dd2af8
Konstantin S. <havendv@gmail.com>
2026-04-29 06:41:30 +0400

    feat: Updated OpenAPI spec (#29)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/FishAudio/Generated/FishAudio.ModelClient.EditModelById.g.cs
+++ src/libs/FishAudio/Generated/FishAudio.ModelClient.EditModelById.g.cs
@@ -133,7 +133,7 @@ namespace FishAudio
                 PrepareEditModelByIdRequest(
                     httpClient: HttpClient,
                     httpRequestMessage: __httpRequest,
-                    id: id,
+                    id: id!,
                     request: request);
 
                 return __httpRequest;
