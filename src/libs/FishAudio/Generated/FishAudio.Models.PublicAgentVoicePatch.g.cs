f291c63238175b38bb2857b25cb0c3c1cc9ae1d0
Konstantin S. <havendv@gmail.com>
2026-08-16 07:44:46 +0400

    feat: Updated OpenAPI spec (#94)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/FishAudio/Generated/FishAudio.Models.PublicAgentVoicePatch.g.cs
+++ src/libs/FishAudio/Generated/FishAudio.Models.PublicAgentVoicePatch.g.cs
@@ -21,6 +21,12 @@ namespace FishAudio
         public global::FishAudio.PublicAgentVoicePatchSpeakingLanguage2? SpeakingLanguage { get; set; }
 
         /// <summary>
+        /// Default Value: openapi-json-null-sentinel-value-2BF93600-0FE4-4250-987A-E5DDB203E464
+        /// </summary>
+        [global::System.Text.Json.Serialization.JsonPropertyName("expressive")]
+        public bool? Expressive { get; set; }
+
+        /// <summary>
         /// Additional properties that are not explicitly defined in the schema
         /// </summary>
         [global::System.Text.Json.Serialization.JsonExtensionData]
@@ -35,15 +41,20 @@ namespace FishAudio
         /// <param name="speakingLanguage">
         /// Default Value: openapi-json-null-sentinel-value-2BF93600-0FE4-4250-987A-E5DDB203E464
         /// </param>
+        /// <param name="expressive">
+        /// Default Value: openapi-json-null-sentinel-value-2BF93600-0FE4-4250-987A-E5DDB203E464
+        /// </param>
 #if NET7_0_OR_GREATER
         [global::System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
 #endif
         public PublicAgentVoicePatch(
             string? voiceId,
-            global::FishAudio.PublicAgentVoicePatchSpeakingLanguage2? speakingLanguage)
+            global::FishAudio.PublicAgentVoicePatchSpeakingLanguage2? speakingLanguage,
+            bool? expressive)
         {
             this.VoiceId = voiceId;
             this.SpeakingLanguage = speakingLanguage;
+            this.Expressive = expressive;
         }
 
         /// <summary>
