0db4a8a0ef0938ecb32f7a3e7c786883356550f7
Konstantin S. <havendv@gmail.com>
2026-08-25 16:30:39 +0400

    test: cover multipart model uploads

---
+++ src/tests/IntegrationTests/ModelClientTests.cs
@@ -0,0 +1,46 @@
+#nullable enable
+
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FishAudio.IntegrationTests;
+
+public partial class Tests
+{
+    [TestMethod]
+    public async Task ModelClient_CreateModel_SendsMultipartBinaryParts()
+    {
+        var handler = new StaticResponseHandler(
+            new HttpResponseMessage(HttpStatusCode.BadRequest)
+            {
+                Content = new StringContent("{\"detail\":\"request captured\"}", Encoding.UTF8, "application/json"),
+            });
+        using var client = new FishAudioClient(
+            "test-api-key",
+            new HttpClient(handler)
+            {
+                BaseAddress = new Uri(FishAudioClient.DefaultBaseUrl),
+            });
+        var request = new CreateModelRequest3
+        {
+            Title = "Multipart model",
+            Voices = new List<byte[]>
+            {
+                new byte[] { 1, 2, 3 },
+                new byte[] { 4, 5, 6 },
+            },
+        };
+
+        var action = () => client.Model.CreateModelAsync(request);
+
+        await action.Should().ThrowAsync<ApiException>();
+        handler.LastRequest.Should().NotBeNull();
+        handler.LastRequest!.RequestUri!.AbsolutePath.Should().Be("/model");
+        handler.LastRequest.Content!.Headers.ContentType!.MediaType.Should().Be("multipart/form-data");
+        handler.LastRequestBody.Should().Contain("name=\"title\"");
+        handler.LastRequestBody.Should().Contain("Multipart model");
+        Regex.Matches(handler.LastRequestBody!, "name=\"voices\"").Should().HaveCount(2);
+        Regex.Matches(handler.LastRequestBody!, "filename=\"file[01]\\.bin\"").Should().HaveCount(2);
+    }
+}
